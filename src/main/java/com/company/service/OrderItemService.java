d12d2d696da7583076329467ea6d9cff553a60f9
Tatsiana Makar <7501480@gmail.com>
2022-08-24 20:45:13 +0300

    Added interfaces OrderItemService  and OrderService

---
+++ src/main/java/com/company/service/OrderItemService.java
@@ -0,0 +1,6 @@
+package com.company.service;
+
+import com.company.repository.entity.OrderItem;
+
+public interface OrderItemService extends AbstractService<OrderItem, Long> {
+}
---
+++ src/main/java/com/company/service/OrderService.java
@@ -0,0 +1,6 @@
+package com.company.service;
+
+import com.company.repository.Order;
+
+public interface OrderService extends AbstractService<Order, Long> {
+}
