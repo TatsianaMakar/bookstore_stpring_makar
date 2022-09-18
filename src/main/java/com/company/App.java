55995f5271963ccb2de2a40971e53bb78526d9e4
Tatsiana Makar <7501480@gmail.com>
2022-09-18 10:59:40 +0300

    added filter to app

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,10 +1,12 @@
 package com.company;
 
 
+import com.company.web.filter.AuthorizationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -30,6 +32,15 @@ public class App implements WebMvcConfigurer {
     @ConfigurationProperties(prefix="spring.second-datasource")
     public DataSource secondaryDataSource() {
         return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public FilterRegistrationBean<AuthorizationFilter> authorizationFilter() {
+        FilterRegistrationBean<AuthorizationFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new AuthorizationFilter());
+        registrationBean.addUrlPatterns("/user/getAll", "/user/delete", "/book/create", "/order/getAll");
+        registrationBean.setOrder(2);
+        return registrationBean;
     }
 
 }
--- src/main/java/com/company/web/filter/AuthorizationFilter.java
+++ src/main/java/com/company/web/filter/AuthorizationFilter.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/user/getAll", "/book/create", "/order/getAll"})
+@WebFilter
 public class AuthorizationFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
