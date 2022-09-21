c7e76f1d7993f9b59c645dc823bd64a6d1b1e46e
Tatsiana Makar <7501480@gmail.com>
2022-09-21 20:59:38 +0300

    Added import DataConfig b WebConfig to App

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,59 +1,18 @@
 package com.company;
 
 
-import com.company.web.filter.AuthorizationFilter;
-import com.company.web.interceptor.MyInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.sql.DataSource;
 
 @SpringBootApplication
+@Import({WebConfig.class,DataConfig.class})
 public class App implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class);
-    }
-
-    @Bean
-   // @Primary
-    @ConfigurationProperties(prefix="spring.datasource")
-    public DataSource primaryDataSource() {
-        return DataSourceBuilder.create().build();
-    }
-
-    @Bean
-    @Primary
-    @ConfigurationProperties(prefix="spring.second-datasource")
-    public DataSource secondaryDataSource() {
-        return DataSourceBuilder.create().build();
-    }
-
-    @Bean
-    public FilterRegistrationBean<AuthorizationFilter> authorizationFilter() {
-        FilterRegistrationBean<AuthorizationFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new AuthorizationFilter());
-        registrationBean.addUrlPatterns("/user/getAll", "/user/delete", "/book/create", "/order/getAll");
-        registrationBean.setOrder(2);
-        return registrationBean;
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(myInterceptor())
-                .addPathPatterns("/**");
-    }
-
-    @Bean
-    public MyInterceptor myInterceptor() {
-        return new MyInterceptor();
     }
 
 }
---
+++ src/main/java/com/company/DataConfig.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataConfig {
+    @Bean
+    // @Primary
+    @ConfigurationProperties(prefix="spring.datasource")
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix="spring.second-datasource")
+    public DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
